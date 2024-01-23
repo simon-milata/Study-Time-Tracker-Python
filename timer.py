f55330970210705efa9cb538fce2ff691a1d46d8
Tamzuu <milata.simon@gmail.com>
2024-01-23 13:54:38 +0200

    Update timer.py
    Created a function for repeating code, wrote comments and made code clearer to read

--- timer.py
+++ timer.py
@@ -3,23 +3,22 @@ import openpyxl as op
 from openpyxl.styles import Font
 import tkinter as tk
 import os
-import atexit
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+#FIND DESKTOP
 desktop = os.path.expanduser("~/Desktop")
 fileName = "Timer Data.xlsx"
 
-if os.path.isfile(desktop + "\\" + fileName):
-    wB = op.load_workbook(desktop + "\\" + fileName)
-    wS = wB.active
-    dataAmount = wS["Z1"].value
-    print("File loaded")
-else:
-    dataAmount = 0
-    wB = op.Workbook()
-    wS = wB.active
+#CREATE TKINTER WINDOW
+window = tk.Tk()
+window.geometry("650x765")
+window.title("Timer")
+
+#RESET DATA + DUMMY GRAPH
+def ResetAndCreate(wS):
+    global dataAmount
     wS.append(["Start Time: ", "End Time: ", "Duration: ", "Break Duration: "])
     wS["X1"].value = "Data amount: "
     wS["A1"].font = Font(bold=True, size=14)
@@ -27,14 +26,73 @@ else:
     wS["C1"].font = Font(bold=True, size=14)
     wS["D1"].font = Font(bold=True, size=14)
     wS["X1"].font = Font(bold=True, size=14)
+    wS["Z1"].value = dataAmount
+
+    fig, ax = plt.subplots()
+
+    ax.bar(datetime.datetime.now().strftime("%d/%m/%Y"), 0)
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Duration in minutes")
+    ax.set_title("Time Spent by Date")
+
+    graphFrame = FigureCanvasTkAgg(fig, master=window)
+    canvas_widget = graphFrame.get_tk_widget()
+    canvas_widget.grid(row=4, column=0, padx=5, pady=10)
+    wB.save(desktop + "\\" + fileName)
+
+#TAKE DATA FROM EXCEL FILE AND APPEND TO LIST
+dateList = []
+durationList = []
+def CollectData():
+    global dataAmount
+    print("Data amount: ", dataAmount)
+    for data in range(2,dataAmount+2):
+        dateList.append(str(wS["B"+str(data)].value).split(" ")[0])
+        if "s" in wS["C"+str(data)].value:
+            durationList.append(int(wS["C"+str(data)].value.replace("s", "")) / 60)
+        else:
+            durationList.append(int(wS["C"+str(data)].value.replace("m", "")))
+    print("Date list: ", dateList)
+    print("Duration list: ", durationList)
+    #GROUP DATA AND DRAW GRAPH
+    data = {"Date": dateList, "Duration": durationList}
+    df = pd.DataFrame(data)
+    grouped_data = df.groupby("Date")["Duration"].sum().reset_index()
+    fig, ax = plt.subplots()
+    ax.bar(grouped_data["Date"], grouped_data["Duration"])
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Duration in minutes")
+    ax.set_title("Time Spent by Date")
+    graphFrame = FigureCanvasTkAgg(fig, master=window)
+    canvas_widget = graphFrame.get_tk_widget()
+    canvas_widget.grid(row=4, column=0, padx=5, pady=10)
+    #CLEAR LISTS
+    dateList.clear()
+    durationList.clear()
+
+#LOAD EXCEL FILE
+if os.path.isfile(desktop + "\\" + fileName):
+    wB = op.load_workbook(desktop + "\\" + fileName)
+    wS = wB.active
+    dataAmount = wS["Z1"].value
+    CollectData()
+    print("File loaded")
+    
+#CREATE EXCEL FILE WITH HEADLINES
+else:
+    dataAmount = 0
+    wB = op.Workbook()
+    wS = wB.active
+    ResetAndCreate(wS)
     print("New file created")
-        
+
 timerRunning = False
 
 startTime = 0
 stopTime = 0
 duration = 0
 
+#SIMPLE STOPWATCH START STOP MECHANISM
 def TimerStartStop():
     global timerRunning
     global startTime
@@ -51,6 +109,7 @@ def TimerStartStop():
     global timerFrame
     global breakFrame
 
+#IF TIMER IS NOT RUNNING RUN IT
     if timerRunning == False:
         timerRunning = True
         timerFrame.config(background="OliveDrab2")
@@ -61,13 +120,14 @@ def TimerStartStop():
 
         breakStartLabel.config(text="Start: 00:00:00")
         breakStopLabel.config(text="Stop: 00:00:00")
-        print("Start:", startTime)
+        print("Timer Start:", startTime)
+#IF TIMER IS RUNNING STOP IT
     else:
         timerRunning = False
         timerFrame.config(background="SystemButtonFace")
         stopTime = datetime.datetime.now()
         timerStopLabel.config(text="Stop: " + str(stopTime.strftime("%H:%M:%S")))
-        print("Stop:", stopTime)
+        print("Timer Stop:", stopTime)
         if breakRunning == True:
             breakRunning = False
             breakFrame.config(background="SystemButtonFace")
@@ -89,6 +149,7 @@ breakStopTime = 0
 breakTimeTotal = 0
 breakTimeToDisplay = 0
 
+#SIMPLE BREAK STOPWATCH MECHANISM
 def BreakStartStop():
     global breakRunning
     global breakTimeTotal
@@ -100,6 +161,7 @@ def BreakStartStop():
     global breakFrame
 
     if timerRunning == True:
+        #IF BREAK NOT RUNNING START BREAK
         if breakRunning == False:
             breakRunning = True
             breakFrame.config(background="OliveDrab2")
@@ -109,6 +171,7 @@ def BreakStartStop():
             breakStopTime = 0
             breakStopLabel.config(text="Stop: 00:00:00")
             print("Break Start:", breakStartTime) 
+            #IF BREAK RUNNING STOP BREAK
         else:
             breakRunning = False
             breakFrame.config(background="SystemButtonFace")
@@ -134,6 +197,7 @@ def SaveData():
     global breakStartTime
     global breakStopTime
     
+    #IF THERE IS START AND STOP DATA SAVE INTO EXCEL
     if startTime != 0 and stopTime != 0:
         dataAmount += 1
         wS["Z1"].value = dataAmount
@@ -162,6 +226,7 @@ def SaveData():
     else:
         print("Error: No data to save")
 
+#DELETE AND CREATE NEW SHEET
 def ResetData():
     global dataAmount
     global timerStartLabel
@@ -176,67 +241,9 @@ def ResetData():
     del wB[wB.active.title]
     wB.create_sheet()
     wS = wB.active
-    wS.append(["Start Time: ", "End Time: ", "Duration: ", "Break Duration: "])
-    wS["X1"].value = "Data amount: "
-    wS["A1"].font = Font(bold=True, size=14)
-    wS["B1"].font = Font(bold=True, size=14)
-    wS["C1"].font = Font(bold=True, size=14)
-    wS["D1"].font = Font(bold=True, size=14)
-    wS["X1"].font = Font(bold=True, size=14)
-    wS["Z1"].value = dataAmount
-    print("Data reset")
-    wB.save(desktop + "\\" + fileName)
+    ResetAndCreate(wS)
 
-    fig, ax = plt.subplots()
-
-    ax.bar(datetime.datetime.now().strftime("%d/%m/%Y"), 0)
-    ax.set_xlabel("Date")
-    ax.set_ylabel("Duration in minutes")
-    ax.set_title("Time Spent by Date")
-
-    graphFrame = FigureCanvasTkAgg(fig, master=window)
-    canvas_widget = graphFrame.get_tk_widget()
-    canvas_widget.grid(row=4, column=0, padx=5, pady=10)
-
-dateList = []
-durationList = []
-def CollectData():
-    global dataAmount
-    print("Data amount: ", dataAmount)
-    for data in range(2,dataAmount+2):
-        dateList.append(str(wS["B"+str(data)].value).split(" ")[0])
-        if "s" in wS["C"+str(data)].value:
-            durationList.append(int(wS["C"+str(data)].value.replace("s", "")) / 60)
-        else:
-            durationList.append(int(wS["C"+str(data)].value.replace("m", "")))
-    print("Date list: ", dateList)
-    print("Duration list: ", durationList)
-    data = {"Date": dateList, "Duration": durationList}
-    df = pd.DataFrame(data)
-
-    grouped_data = df.groupby("Date")["Duration"].sum().reset_index()
-
-    fig, ax = plt.subplots()
-
-    ax.bar(grouped_data["Date"], grouped_data["Duration"])
-    ax.set_xlabel("Date")
-    ax.set_ylabel("Duration in minutes")
-    ax.set_title("Time Spent by Date")
-
-    graphFrame = FigureCanvasTkAgg(fig, master=window)
-    canvas_widget = graphFrame.get_tk_widget()
-    canvas_widget.grid(row=4, column=0, padx=5, pady=10)
-
-
-    dateList.clear()
-    durationList.clear()
-
-window = tk.Tk()
-window.geometry("650x765")
-window.title("Timer")
-
-# TIMER
-
+#TIMER UI ROW
 timerFrame = tk.Frame(window, highlightbackground="black", highlightthickness=2)
 timerFrame.grid(row=0, column=0, padx=10, pady=10)
 timerLabel = tk.Label(timerFrame, text="Timer: ", font="Calibri 16")
@@ -249,8 +256,7 @@ timerStartLabel.grid(row=0, column=2, padx=5, pady=10)
 timerStopLabel = tk.Label(timerFrame, text="Stop: 00:00:00", font="Calibri 16")
 timerStopLabel.grid(row=0, column=3, padx=5, pady=10)
 
-# BREAK
-
+#BREAK TIMER UI ROW
 breakFrame = tk.Frame(window, highlightbackground="black", highlightthickness=2)
 breakFrame.grid(row=1, column=0, padx=10, pady=10)
 breakLabel = tk.Label(breakFrame, text="Break: ", font="Calibri 16")
@@ -263,25 +269,12 @@ breakStartLabel.grid(row=0, column=2, padx=5, pady=10)
 breakStopLabel = tk.Label(breakFrame, text="Stop: 00:00:00", font="Calibri 16")
 breakStopLabel.grid(row=0, column=3, padx=5, pady=10)
 
-# DATA
-
+#DATA UI ROW
 dataFrame = tk.Frame(window, highlightbackground="black", highlightthickness=2)
 dataFrame.grid(row=3, column=0, padx=10, pady=10)
 saveDataBtn = tk.Button(dataFrame, text="Save Data", command=SaveData, font="Calibri 16")
 saveDataBtn.grid(row=0, column=0, padx=5, pady=10)
 resetDataBtn = tk.Button(dataFrame, text="Reset Data", command=ResetData, font="Calibri 16")
 resetDataBtn.grid(row=0, column=1, padx=5, pady=10)
-
-
-fig, ax = plt.subplots()
-
-ax.bar(datetime.datetime.now().strftime("%d/%m/%Y"), 0)
-ax.set_xlabel("Date")
-ax.set_ylabel("Duration in minutes")
-ax.set_title("Time Spent by Date")
-
-graphFrame = FigureCanvasTkAgg(fig, master=window)
-canvas_widget = graphFrame.get_tk_widget()
-canvas_widget.grid(row=4, column=0, padx=5, pady=10)
 
 window.mainloop()
